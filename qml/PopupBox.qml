b2dfdc520b0ce3f1ce326f853b427bb893a873d4
Frederi Mahieu <frederi.mahieu@gmail.com>
2023-05-06 12:14:54 +0200

    Version simplifiee pour poster sur page php perso sans bdd.

--- qml/PopupBox.qml
+++ qml/PopupBox.qml
@@ -1,6 +1,6 @@
-import QtQuick 2.10
-import QtQuick.Controls 2.3
-import QtQuick.Controls.Material 2.1
+import QtQuick
+import QtQuick.Controls
+import QtQuick.Controls.Material
 
 
 Dialog {
--- src/cloud.h
+++ src/cloud.h
@@ -12,12 +12,7 @@ class Cloud : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(int linkStatus READ linkStatus NOTIFY linkStatusChanged) // -1 Fail, 0: warning, 1:OK
-    Q_PROPERTY(QStringList timeChooserList READ timeChooserList CONSTANT)
-    Q_PROPERTY(int timeChooserIndex READ timeChooserIndex NOTIFY timeChooserIndexChanged)
-    Q_PROPERTY(QDate eventDate READ eventDate NOTIFY eventDateChanged)
     Q_PROPERTY(QString urlScores READ urlScores NOTIFY urlScoresChanged)
-    Q_PROPERTY(QString urlMarques READ urlMarques NOTIFY urlMarquesChanged)
-    Q_PROPERTY(QString userId READ userId NOTIFY userIdChanged)
     Q_PROPERTY(QString password READ password NOTIFY passwordChanged)
     Q_PROPERTY(QString eventName READ eventName NOTIFY eventNameChanged)
 
@@ -26,12 +21,7 @@ public:
     explicit Cloud();
 
     int linkStatus() const { return _linkStatus; }
-    QStringList timeChooserList() const { return _timeChooserList; }
-    int timeChooserIndex() const { return _timeChooserIndex; }
-    QDate eventDate() const { return _eventDate; }
     QString urlScores() const { return _urlScores; }
-    QString urlMarques() const { return _urlMarques; }
-    QString userId() const { return _userId; }
     QString password() const { return _password; }
     QString eventName() const { return _eventName; }
 
@@ -41,27 +31,16 @@ signals:
     void log(QString logString);
     void logError(int icon, QString title, QString content);
     void linkStatusChanged(int linkStatus);
-    void timeChooserIndexChanged(int timeChooserIndex);
-    void eventDateChanged(QDate eventDate);
     void urlScoresChanged(QString urlScores);
-    void urlMarquesChanged(QString urlMarques);
-    void userIdChanged(QString userId);
     void passwordChanged(QString password);
     void eventNameChanged(QString eventName);
 
 
 public slots:
     void postJsonDoc(QJsonDocument jsonDoc);
-    void setTimeChooserIndex(int index);
-    void setEventDate(const QDate& date);
-    void setUserId(const QString& userId);
     void setPassword(const QString& password);
     void setUrlScores(const QString& urlScores);
-    void setUrlMarques(const QString& urlMarques);
     void setEventName(const QString& eventName);
-    void publish();
-    void hide();
-
 
 private slots:
 
@@ -72,12 +51,7 @@ private:
 
     QNetworkAccessManager* _networkManager;
     int _linkStatus;
-    QStringList _timeChooserList;
-    int _timeChooserIndex;
-    QDate _eventDate;
     QString _urlScores;
-    QString _urlMarques;
-    QString _userId;
     QString _password;
     QString _eventName;
 
